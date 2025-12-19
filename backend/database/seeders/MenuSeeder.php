463a160ac361b24eb89b04124067f770f54e2d1e
fqueta <ger.maisaqui1@gmail.com>
2025-12-19 05:36:51 -0300

    15/12/2025

--- backend/database/seeders/MenuSeeder.php
+++ backend/database/seeders/MenuSeeder.php
@@ -44,45 +44,59 @@ class MenuSeeder extends Seeder
          */
         DB::table('permissions')->truncate();
         DB::table('permissions')->insert([
-            // MASTER → acesso a tudo
-            [
-                'name' => 'Master',
-                'description' => 'Desenvolvedores',
-                'redirect_login' => '/home',
-                'active' => 's',
-            ],
+                // MASTER → acesso a tudo
+                [
+                    'name' => 'Master',
+                    'description' => 'Desenvolvedores',
+                    'redirect_login' => '/home',
+                    'active' => 's',
+                ],
 
-            // ADMINISTRADOR → tudo, mas em configurações só "Usuários" e "Perfis"
-            [
-                'name' => 'Administrador',
-                'description' => 'Administradores do sistema',
-                'redirect_login' => '/home',
-                'active' => 's'
-            ],
+                // ADMINISTRADOR → tudo, mas em configurações só "Usuários" e "Perfis"
+                [
+                    'name' => 'Administrador',
+                    'description' => 'Administradores do sistema',
+                    'redirect_login' => '/home',
+                    'active' => 's'
+                ],
 
-            // GERENTE → todos os menus exceto configurações
-            [
-                'name' => 'Gerente',
-                'description' => 'Gerente do sistema (sem acesso a configurações)',
-                'redirect_login' => '/home',
-                'active' => 's'
-            ],
+                // GERENTE → todos os menus exceto configurações
+                [
+                    'name' => 'Auxiliar Adminstrativo',
+                    'description' => 'Gerente do sistema (sem acesso a configurações)',
+                    'redirect_login' => '/home',
+                    'active' => 's'
+                ],
 
-            // ESCRITÓRIO → somente dois primeiros menus
-            [
-                'name' => 'Escritório',
-                'description' => 'Acesso limitado a Dashboard e Clientes',
-                'redirect_login' => '/home',
-                'active' => 's'
-            ],
-            // Cliente → para clientes sem acesso ao admin
-            [
-                'name' => 'Cliente',
-                'description' => 'Acesso limitado a Dashboard e Clientes',
-                'redirect_login' => '/home',
-                'active' => 's'
-            ],
-        ]);
+                // ESCRITÓRIO → somente dois primeiros menus
+                [
+                    'name' => 'Escritório',
+                    'description' => 'Acesso limitado a Dashboard e Clientes',
+                    'redirect_login' => '/home',
+                    'active' => 's'
+                ],
+                // CONSULTOR → somente dois primeiros menus
+                [
+                    'name' => 'Consultor',
+                    'description' => 'Consultores do sistema',
+                    'redirect_login' => '/home',
+                    'active' => 's'
+                ],
+                // INSTRUTOR → somente dois primeiros menus
+                [
+                    'name' => 'Instrutor',
+                    'description' => 'Instrutores do sistema',
+                    'redirect_login' => '/home',
+                    'active' => 's'
+                ],
+                // Cliente → para clientes sem acesso ao admin
+                [
+                    'name' => 'Cliente',
+                    'description' => 'Sem acesso ao Dashboard porem com acesso ao painel de Clientes',
+                    'redirect_login' => '/home',
+                    'active' => 's'
+                ],
+            ]);
 
         // Recria vínculos de permissão para todos os menus
         $menusCollection = Menu::all();
